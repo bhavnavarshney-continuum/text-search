5610bca6885000e62bdf5f58557d609cabdbde39
bhavna <bhavna.varshney@continuum.net>
2020-04-23 17:06:14 +0000

    Added external library support

--- approach.go
+++ approach.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/buger/jsonparser"
+	gojsonq "github.com/thedevsaddam/gojsonq/v2"
 )
 
 type DeviceData struct {
@@ -19,7 +20,7 @@ type DeviceData struct {
 		ClosestPreviousTaskRunDate  int         `json:"closestPreviousTaskRunDate"`
 		ClosestPreviousTaskStatus   bool        `json:"closestPreviousTaskStatus"`
 		CriticalTicketsCnt          int         `json:"criticalTicketsCnt"`
-		CurrentEndpointAgentVersion string      `json:"currentEndpointAgentVersion"`
+		CurrentEndpointAgentVersion float64     `json:"currentEndpointAgentVersion"`
 		EndPointID                  int         `json:"endPointId"`
 		FriendlyName                string      `json:"friendlyName"`
 		HbAgentDateTimeUTC          int         `json:"hbAgentDateTimeUTC"`
@@ -49,18 +50,57 @@ type DeviceData struct {
 
 func unmarshalJSON() (DeviceData, error) {
 	model := DeviceData{}
-	err := json.Unmarshal([]byte(jsonData10k), &model)
+	err := json.Unmarshal([]byte(jsonData10K), &model)
 	if err != nil {
 		return DeviceData{}, err
 	}
 	return model, nil
 }
 
-func improvedUnmarshalling() {
+func goJSONQ(hint string) {
 	start := time.Now()
-	_, err := jsonparser.ArrayEach([]byte(jsonData30K), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+	q1 := gojsonq.New().FromString(jsonData60K).From("items").Where("siteName", "=", hint)
+	q2 := gojsonq.New().FromString(jsonData60K).From("items").Where("osProduct", "=", hint)
+	q3 := gojsonq.New().FromString(jsonData60K).From("items").Where("friendlyName", "=", hint)
+	q4 := gojsonq.New().FromString(jsonData60K).From("items").Where("machineName", "=", hint)
+	q5 := gojsonq.New().FromString(jsonData60K).From("items").Where("resourceType", "=", hint)
+
+	elapsed := time.Since(start)
+	fmt.Printf("\nGoJSONQ took %v time", elapsed)
+
+	fmt.Printf("\nSiteName:%v, OsProduct:%v, FriendlyName:%v, MachineName:%v,ResourceType:%v ", q1.Count(), q2.Count(), q3.Count(), q4.Count(), q5.Count())
+}
+
+// func jsonParserLibrary() {
+// 	start := time.Now()
+// 	_, err := jsonparser.ArrayEach([]byte(jsonData30K), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+// 		//fmt.Println(jsonparser.Get(value, "closestNextTaskName"))
+// 		jsonparser.Get(value, "closestNextTaskName")
+// 	}, "items")
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	//fmt.Println(string(val))
+// 	elapsed := time.Since(start)
+// 	fmt.Println("\nJsonParser took %v time", elapsed)
+// }
+
+func jsonParserLibrary(hint string) {
+	start := time.Now()
+	c := 0
+	_, err := jsonparser.ArrayEach([]byte(jsonData1K), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
 		//fmt.Println(jsonparser.Get(value, "closestNextTaskName"))
-		jsonparser.Get(value, "closestNextTaskName")
+		osProduct, _, _, _ := jsonparser.Get(value, "osProduct")
+		siteName, _, _, _ := jsonparser.Get(value, "siteName")
+		friendlyName, _, _, _ := jsonparser.Get(value, "friendlyName")
+		machineName, _, _, _ := jsonparser.Get(value, "machineName")
+		resourceType, _, _, _ := jsonparser.Get(value, "resourceType")
+
+		if string(osProduct) == hint || string(siteName) == hint || string(friendlyName) == hint || string(machineName) == hint || string(resourceType) == hint {
+			c++
+		}
 	}, "items")
 
 	if err != nil {
@@ -69,7 +109,8 @@ func improvedUnmarshalling() {
 	}
 	//fmt.Println(string(val))
 	elapsed := time.Since(start)
-	fmt.Println("\nNaive Search took %v time", elapsed)
+	fmt.Printf("\n Match found in %v records", c)
+	fmt.Printf("\nJsonParser took %v time", elapsed)
 }
 
 func naiveSearch(hint string) {
@@ -90,6 +131,6 @@ func naiveSearch(hint string) {
 	}
 
 	elapsed := time.Since(start)
-	fmt.Printf("%+v", result)
+	fmt.Printf("Match Found in %v", len(result))
 	fmt.Println("\nNaive Search took %v time", elapsed)
 }
--- main.go
+++ main.go
@@ -4,19 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
+	"os"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	rest()
+	//cmd()
+}
+
+func rest() {
 	router := mux.NewRouter()
 	router.HandleFunc("/search/{hint}", searchText).Methods("GET")
 	http.ListenAndServe("localhost:8090", router)
 }
 
+func cmd() {
+	hint := os.Args[1]
+	fmt.Println("Searching:", hint)
+	naiveSearch(hint)
+	jsonParserLibrary(hint)
+	goJSONQ(hint)
+}
+
 func searchText(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	fmt.Println("Searching:", vars["hint"])
-	naiveSearch(vars["hint"])
-	//improvedUnmarshalling()
+
+	fmt.Println("\nSearching:", vars["hint"])
+	//naiveSearch(vars["hint"])
+	//jsonParserLibrary(vars["hint"])
+	goJSONQ(vars["hint"])
 }
